8e5295d9b769a9178c2c5cf1f3a1ea4e309c5131
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-07-29 23:02:34 +0200

    Registra il plugin WhatsApp nativo

--- android/app/src/main/java/it/vargacantieri/hera/MainActivity.java
+++ android/app/src/main/java/it/vargacantieri/hera/MainActivity.java
@@ -6,12 +6,14 @@ import com.getcapacitor.BridgeActivity;
 
 import it.vargacantieri.hera.geofence.HeraGeofencePlugin;
 import it.vargacantieri.hera.biometric.HeraBiometricPlugin;
+import it.vargacantieri.hera.whatsapp.HeraWhatsAppPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(HeraGeofencePlugin.class);
         registerPlugin(HeraBiometricPlugin.class);
+        registerPlugin(HeraWhatsAppPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }

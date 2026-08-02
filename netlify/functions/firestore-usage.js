bef2285ce17d7b50ac2f06f1dbc56d53f2662b6c
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-08-02 10:19:55 +0200

    Ripristina il grant OAuth corretto

--- netlify/functions/firestore-usage.js
+++ netlify/functions/firestore-usage.js
@@ -62,7 +62,7 @@ async function accessToken() {
   const response = await fetch("https://oauth2.googleapis.com/token", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({ grant_type: "urn:ietf:params:oauth2:grant-type:jwt-bearer", assertion })
+    body: new URLSearchParams({ grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer", assertion })
   });
   const data = await response.json().catch(() => ({}));
   if (!response.ok || !data.access_token) throw new Error(data.error_description || "Autorizzazione Cloud Monitoring non riuscita.");

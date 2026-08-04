cbb96ffceffb428bbf1b88c1337cb89cac3dc75b
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-08-04 15:09:54 +0200

    Esporta trigger viste operative

--- functions/main.js
+++ functions/main.js
@@ -4,11 +4,13 @@ const existingFunctions = require("./index");
 const userNotificationFunctions = require("./user-notifications");
 const centralNotificationFunctions = require("./central-notifications");
 const sharedCalendarViewFunctions = require("./shared-calendar-view");
+const sharedOperationalViewFunctions = require("./shared-operational-views");
 
 Object.assign(
   exports,
   existingFunctions,
   userNotificationFunctions,
   centralNotificationFunctions,
-  sharedCalendarViewFunctions
+  sharedCalendarViewFunctions,
+  sharedOperationalViewFunctions
 );

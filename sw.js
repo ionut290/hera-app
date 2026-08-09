a9a3fb8189a391a4d076c64d83a760aad3bf1aee
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-08-09 13:47:02 +0200

    Aggiorna cache schermata login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "varga-cantieri-shell-v111";
+const CACHE_NAME = "varga-cantieri-shell-v112";
 
 // Solo i file indispensabili per mostrare rapidamente login, Home e commesse.
 // Un errore in un modulo accessorio non deve più bloccare l'installazione del Service Worker.

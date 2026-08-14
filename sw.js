50a4c5047def5150ae8114cd616a8d2812e049b3
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-08-14 17:14:29 +0200

    Align service worker reset version with registration

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "varga-cantieri-shell-v129";
-const CACHE_RESET_VERSION = "20260814-authflow1";
+const CACHE_RESET_VERSION = "20260814-loading-humor1";
 const APP_SHELL = [
   "./",
   "./index.html",

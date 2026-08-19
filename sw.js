018d25098935d950780e1b8c4e888dc0c939f29a
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-08-19 18:18:36 +0200

    Mantiene allineata la versione PWA gestita dal deploy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "varga-cantieri-shell-v137";
-const CACHE_RESET_VERSION = "20260819-data-safety1";
+const CACHE_NAME = "varga-cantieri-shell-v136";
+const CACHE_RESET_VERSION = "20260814-loading-humor1";
 const APP_SHELL = [
   "./",
   "./index.html",

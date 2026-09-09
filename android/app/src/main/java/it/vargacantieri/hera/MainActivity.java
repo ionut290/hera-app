eb57ef080af1513d91f893115e9551ea8b375eb2
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-09-09 07:37:46 +0200

    Corregge visibilità del lifecycle Android

--- android/app/src/main/java/it/vargacantieri/hera/MainActivity.java
+++ android/app/src/main/java/it/vargacantieri/hera/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         enforceFullscreenWithoutActionBar();
     }
